353603c1596dfde475cdbaec3651439d5701bc33
madhunadamala <madhu.nadamala@gmail.com>
2020-07-17 20:35:18 -0700

    new files added

---
+++ python_work/Readme.txt
@@ -0,0 +1,1 @@
+All this samples are using the Book "Python Crash Course 2nd Edition in Kindle"
---
+++ python_work/full_name.py
@@ -0,0 +1,4 @@
+first_name="Madhusudhana"
+last_name="Nadamala"
+fullname=f"{first_name.upper()} {last_name.lower()} Reddy"
+print(fullname)
