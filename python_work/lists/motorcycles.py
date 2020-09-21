dc5df2d185da9ac8e0dd3fca78483646da87be67
madhunadamala <madhu.nadamala@gmail.com>
2020-09-21 13:35:31 -0700

    changes

--- python_work/lists/motorcycles.py
+++ python_work/lists/motorcycles.py
@@ -34,3 +34,28 @@ print(motorcycles)
 popped_motorcycle = motorcycles.pop()
 print(motorcycles)
 print(popped_motorcycle)
+
+#How pop is userful
+motorcycles = ['honda','yamaha','suzuki']
+last_owned = motorcycles.pop()
+print(f"The last motorcycle I owned was a {last_owned.title()}.")
+
+#Popping Items from nay position in a List
+motorcycles = ['honda','yamaha','suzuki']
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
+
+#Remove an item by value
+motorcycles = ['honda','yamaha','suzuki','ducati']
+print(motorcycles)
+
+motorcycles.remove('ducati')
+print(motorcycles)
+
+motorcycles = ['honda','yamaha','suzuki','ducati']
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print (motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
