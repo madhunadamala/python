da7336687407379e6d7a956296d4b48b0032afb8
madhunadamala <madhu.nadamala@gmail.com>
2021-04-18 20:28:06 -0700

    List Comprehension added

--- python_work/lists/chapter_4_loops/04-squares.py
+++ python_work/lists/chapter_4_loops/04-squares.py
@@ -10,3 +10,7 @@ squares = []
 for value in range(1,15):
 	squares.append(value**2)
 print(squares)
+
+# Another way to write using "List Comprehension"
+squares = [value**2 for value in range(1,16)]
+print(squares)
