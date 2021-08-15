13bdd03290417c3a537c89b6ec41e2ada7179dad
Madhu Nadamala <madhu.nadamala@outlook.com>
2021-08-14 22:28:04 -0700

    New file added

---
+++ python_work/02_Variables_DataTypes/apostrophe.py
@@ -0,0 +1,3 @@
+message = "One of the Python's strengths is its diverse community"
+print(message)
+
--- python_work/02_Variables_DataTypes/full_name.py
+++ python_work/02_Variables_DataTypes/full_name.py
@@ -1,5 +1,21 @@
-first_name="Madhusudhana"
-last_name="Nadamala"
-middle_name="Reddy"
-fullname=f" {last_name.upper()} {first_name.title()} {middle_name.lower()}"
+first_name = "Madhusudhana"
+last_name = "Nadamala"
+middle_name = "Reddy"
+fullname = f" {last_name.upper()} {first_name.title()} {middle_name.lower()}"
 print(fullname)
+print(f"Hello {fullname}!")
+
+# Tab to the line
+print("\tPython")
+
+# To add new Line
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+# Trimming - Test these in Terminal for better understanding. Do not use Print command in Terminal
+favorite_language = ' python '
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+
+
