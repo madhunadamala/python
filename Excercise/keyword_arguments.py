0340f5e6b2e4a42a42a731e501eac16d14696a2a
Madhu Nadamala <madhu.nadamala@outlook.com>
2024-06-09 09:27:27 -0700

    CheatSheet

---
+++ Excercise/keyword_arguments.py
@@ -0,0 +1,6 @@
+# Define function with Parameters
+def product_info(productname, price):
+	print("producname: " + productname)
+	print("price " + str(price))
+
+product_info(price=45, productname="White T-shirt")
---
+++ Excercise/name.py
@@ -0,0 +1,4 @@
+def name():
+	print("What is your name?")
+
+name()
